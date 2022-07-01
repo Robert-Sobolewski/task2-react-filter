476e2ba84182e7661f39ba2154528dc6e4536553
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2022-07-01 18:29:45 +0200

    setup commit

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const Filter = () => {
+  return (
+    <div className="filter-container">
+        <input role="textbox" type="text" />
+        <ul role="list">
+            <li role="listitem"></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Filter
---
+++ src/hooks/useFilter.jsx
@@ -0,0 +1,5 @@
+
+const useFilter = () => {}
+
+
+export default useFilter;
