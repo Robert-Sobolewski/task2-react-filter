37390a51ca90292e1f7fda5abb48fbff06aef545
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2022-07-01 19:53:48 +0200

    done

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,12 +1,22 @@
 
-import React from 'react'
-
+import React, { useState } from 'react'
+import users from '../../data/users.json'
+import useFilter from '../../hooks/useFilter'
 const Filter = () => {
+    const [search, setSearchValue] = useState("")
   return (
     <div className="filter-container">
-        <input role="textbox" type="text" />
+        <input role="textbox" type="text"
+        value={search}
+        onChange={(e)=> setSearchValue(e.target.value)}
+        />
         <ul role="list">
-            <li role="listitem"></li>
+            {
+                useFilter(search).map((user,index)=>(
+                    <li key={index} role="listitem">{user.name}</li>
+                ))
+            }
+            {/* <li role="listitem"></li> */}
         </ul>
     </div>
   )
--- src/hooks/useFilter.jsx
+++ src/hooks/useFilter.jsx
@@ -1,5 +1,12 @@
-
-const useFilter = () => {}
+import users from '../data/users.json';
+const useFilter = (searchValue) => {
+    return searchValue.length === ""?
+    Array.from(users.map(user => user.name ))
+    :
+    users.filter( user =>
+        user.name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+}
 
 
 export default useFilter;
