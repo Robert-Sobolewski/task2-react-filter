a5edcaf07dbd25dfc53641acd022489204731f96
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2022-07-04 12:15:27 +0200

    solution commit

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,14 +1,32 @@
 
-import React, { useState } from 'react'
-import users from '../../data/users.json'
+import React, { useCallback, useState } from 'react'
+
 import useFilter from '../../hooks/useFilter'
 const Filter = () => {
     const [search, setSearchValue] = useState("")
+    
+    /** Implement debounce function
+     * @param funct - function to be executed
+     * @param delay - number of milliseconds, default is 500
+     */
+    const debounce = (funct, delay=500) =>{
+        let timer;
+        console.log("call debounce")
+        return function(...args){
+            const context = this;
+            if(timer) clearTimeout(timer)
+            timer = setTimeout(() =>{
+                timer = null;
+                funct.apply(context, args)
+            },delay)
+        }
+    }
+    const optimizedSearch = useCallback(debounce(setSearchValue, 200),[])
   return (
     <div className="filter-container">
         <input role="textbox" type="text"
         value={search}
-        onChange={(e)=> setSearchValue(e.target.value)}
+        onChange={e=> optimizedSearch(e.target.value)}
         />
         <ul role="list">
             {
--- src/components/filter/Filter.spec.js
+++ src/components/filter/Filter.spec.js
@@ -1,16 +1,13 @@
-import { render, screen, waitFor } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import React from 'react'
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import React from "react";
 
+import Filter from "./Filter";
 
-import Filter from "./Filter"
+describe("test filter component", () => {
+  test("render filter component", () => {
+    render(<Filter />);
 
-describe('test filter component',()=>{
-
-    test('render filter component',()=>{
-        render(<Filter />)
-
-            //* get filter elements
+    //* get filter elements
     const filterInput = screen.getByRole("textbox");
     const filteredUsers = screen.getByRole("list");
     const users = screen.getAllByRole("listitem");
@@ -19,29 +16,28 @@ describe('test filter component',()=>{
     expect(filterInput).toBeInTheDocument();
     expect(filteredUsers).toBeInTheDocument();
     expect(users).toHaveLength(10);
-    })
+  });
 
-    test("should set input value and filter values", async () => {
-        render(<Filter />);
-    
-        //* get filter elements
-        const filterInput = screen.getByRole("textbox");
-        const unfilteredUsers = screen.getAllByRole("listitem");
-    
-        //* should have 10 users
-        expect(unfilteredUsers).toHaveLength(10);
-    
-        //* set input value
-        await userEvent.type(filterInput, "le");
-    
-        //* get filtered users
-        await waitFor(() => {
-          const filteredUsers = screen.getAllByRole("listitem");
-          expect(filteredUsers).toHaveLength(5);
-        });
-    
-        //* should have filter
-        expect(filterInput.value).toBe("le");
-      });
+  test("should set input value and filter values", async () => {
+    render(<Filter />);
 
-})
+    //* get filter elements
+    const filterInput = screen.getByRole("textbox");
+    const unfilteredUsers = screen.getAllByRole("listitem");
+
+    //* should have 10 users
+    expect(unfilteredUsers).toHaveLength(10);
+
+    //* set input value
+    fireEvent.change(filterInput, { target: { value: "le" } });
+
+    //* get filtered users
+    await waitFor(() => {
+      const filteredUsers = screen.getAllByRole("listitem");
+      expect(filteredUsers).toHaveLength(5);
+    });
+
+    //* should have filter
+    expect(filterInput.value).toBe("le");
+  });
+});
